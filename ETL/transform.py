d8cba8833921f50323b51878d0f9425a82046382
Ignacio V Perez <igp@lunar.app>
2022-12-16 23:42:44 +0100

    chore: refactor transform.py

--- ETL/transform.py
+++ ETL/transform.py
@@ -6,19 +6,16 @@ def storeRespDataNP(
     reqTimestamp,
     response,
 ):
+    root = response["rows"][0]["elements"][0]
     TravelTime.setTimeStamps(reqTimestamp)
-    TravelTime.distanceAVG.append(
-        round(response["rows"][0]["elements"][0]["distance"]["value"] / 1000.0, 2)
-    )
+    TravelTime.distanceAVG.append(round(root["distance"]["value"] / 1000.0, 2))
     TravelTime.durationInclTraffic.append(
         round(
-            response["rows"][0]["elements"][0]["duration_in_traffic"]["value"] / 60.0,
+            root["duration_in_traffic"]["value"] / 60.0,
             2,
         )
     )
-    TravelTime.durationEnclTraffic.append(
-        round(response["rows"][0]["elements"][0]["duration"]["value"] / 60.0, 2)
-    )
+    TravelTime.durationEnclTraffic.append(round(root["duration"]["value"] / 60.0, 2))
 
 
 def travelTimeColumnStack(TravelTime):
