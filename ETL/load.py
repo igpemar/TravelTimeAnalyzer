115cf5d460743a845e363ef88d595e5d6697b876
Ignacio V Perez <igp@lunar.app>
2022-12-16 18:35:58 +0100

    add: implement load part of the ETL pipeline

---
+++ ETL/load.py
@@ -0,0 +1,32 @@
+import numpy as np
+import ETL.transform
+
+
+def saveTravelStats2txt(TravelStats, destination="Output"):
+    MyList1 = ETL.transform.travelTimeColumnStack(TravelStats.home2work)
+    MyList2 = ETL.transform.travelTimeColumnStack(TravelStats.work2home)
+    print(
+        TravelStats.home2work.timestampSTR[-1]
+        + " ; --> Dumping response data to output file..."
+    )
+    np.savetxt(
+        destination + "_h2w.csv",
+        MyList1,
+        fmt="%s",
+        delimiter=" ; ",
+        comments="",
+        header="Req #. ; Timestamp ; Distance [km] ; Duration (incl.traffic) [min] ; Duration (excl.traffic) [min]",
+    )
+    np.savetxt(
+        destination + "_w2h.csv",
+        MyList2,
+        fmt="%s",
+        delimiter=" ; ",
+        comments="",
+        header="Req #. ; Timestamp ; Distance [km] ; Duration (incl.traffic) [min] ; Duration (excl.traffic) [min]",
+    )
+    print(TravelStats.home2work.timestampSTR[-1] + " ; ---> DONE!")
+    print(
+        str(TravelStats.home2work.timestampSTR[-1])
+        + " ; ----------------------------------------------"
+    )
