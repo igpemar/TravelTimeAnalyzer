2b901182fe0ee709cd13427ab67516dc89316160
Ignacio V Perez <igp@lunar.app>
2022-12-18 21:25:46 +0100

    change: encapsulate entire ETR pipeline in function in preparation for threading

--- main.py
+++ main.py
@@ -8,56 +8,28 @@ import helpers.config, helpers.printers
 import ETL.extract, ETL.transform, ETL.load
 
 REQ_SEND = 0
-RESTART_INPUT = ""
+RESTART_INPUT = "Y"
 
-if __name__ == "__main__":
-    # Get configuration variables
-    config = helpers.config.Config()
-    config.initiateAPIkey()
-    API_KEY = config.getApiKey()
 
-    # Print intro message
-    helpers.printers.printIntroMessage(config)
-
-    # Checking for restart
-    print("Checking for restart...")
-    (TravelStats, Restart) = ETL.extract.restart_check(RESTART_INPUT)
-
-    # Checking for start time
-    while helpers.config.isItTimeToStart(config.START_TIME):
-        helpers.printers.printWaitTimeMessage(config.START_TIME)
-        time.sleep(15)
-
-    # Initializing Variables
-    if Restart == 1:
-        MyList1, MyList2 = [], []
-
-    t0, dtDataDump = datetime.datetime.now(), datetime.datetime.now()
-
-    # Building request
-    h2wRequest, w2hRequest = ETL.extract.build_request(config)
-
-    prev_week = t0.isocalendar()[1]
-    print("Starting now at: ", t0)
-
-    # Entering request loop
+def startETLPipeline(
+    TravelStats: ETL.extract.TravelStats, config: helpers.config.Config
+) -> None:
+    lastDataDump = datetime.datetime.now()
     while True:
         # Dealing with timestamps
         reqTimestamp = datetime.datetime.now()
-        Current_Week = reqTimestamp.isocalendar()[1]
 
         # Sending Requests
-        payload, headers = {}, {}
         reqID_1 = TravelStats.home2work.reqID[-1]
         reqID_2 = TravelStats.work2home.reqID[-1]
         if REQ_SEND == 1:
             # Sending request for HOME2WORK
             helpers.printers.printRequestSent(reqID_1)
-            h2wResp = ETL.extract.sendRequest(config, h2wRequest, reqID_1)
+            h2wResp = ETL.extract.sendRequest(config, reqs.h2wRequest, reqID_1)
 
             # Sending request for WORK2HOME
             helpers.printers.printRequestSent(reqID_2)
-            w2hResp = ETL.extract.sendRequest(config, w2hRequest, reqID_2)
+            w2hResp = ETL.extract.sendRequest(config, reqs.w2hRequest, reqID_2)
 
             # Parsing response
             h2wRespJSON = h2wResp.json()
@@ -75,14 +47,41 @@ if __name__ == "__main__":
         ETL.transform.storeRespDataNP(TravelStats.work2home, reqTimestamp, w2hRespJSON)
 
         # Persisting data in disk
-        timeSinceLastDataDump = reqTimestamp - dtDataDump
+        timeSinceLastDataDump = reqTimestamp - lastDataDump
         if reqID_1 == 1 or helpers.config.isItTimeToDumpData(
             timeSinceLastDataDump, config
         ):
             ETL.load.saveTravelStats2txt(TravelStats)
-            dtDataDump = datetime.datetime.now()
+            lastDataDump = datetime.datetime.now()
 
         # Incrementing request numbers
         TravelStats.incrementRequestIDs(2)
 
         helpers.config.waitForNextCycle(reqTimestamp, config)
+    return
+
+
+if __name__ == "__main__":
+    # Get configuration variables
+    config = helpers.config.Config()
+
+    # Print intro message
+    helpers.printers.printIntroMessage(config)
+
+    # Checking for restart
+    print("Checking for restart...")
+    TravelStats = ETL.extract.restart_check(RESTART_INPUT)
+
+    # Checking for start time
+    while helpers.config.isItTimeToStart(config.START_TIME):
+        helpers.printers.printWaitTimeMessage(config.START_TIME)
+        time.sleep(15)
+
+    # Building request
+    reqs = ETL.extract.GoogleMapsRequests()
+    reqs.build_request(config)
+
+    print("Starting now at: ", datetime.datetime.now())
+
+    # Start ETL Pipeline
+    startETLPipeline(TravelStats, config)
