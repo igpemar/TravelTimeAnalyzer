4f34b7061ab1285502a8a375f792aa9e04cbc6f3
Ignacio V Perez <igp@lunar.app>
2023-01-01 17:29:14 +0100

    add: introducing dockerrun.sh

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,7 +1,8 @@
+REM docker network create -d bridge mynetwork
 docker stop tta_db;
-docker rm tta_db
-docker run --name tta_db -d -p 5431:5431 -e POSTGRES_PASSWORD=postgres -e POSTGRESS_NAME=travelanalyzer postgres;
+docker rm tta_db;
+docker run --name tta_db -d -p 5433:5431 --network mynetwork -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=travelAnalyzer postgres -p 5431;
 docker stop tta_app;
-docker rm tta_app
+docker rm tta_app;
 docker build -t tta_app ./;
-docker run --name tta_app -d tta_app;
+docker run --name tta_app -d --network mynetwork tta_app;
