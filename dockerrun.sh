727fff4aae1c56ae289e91892d4e050b637c35df
Ignacio V Perez <igp@lunar.app>
2023-01-01 18:02:15 +0100

    add: commented line in dockerrun.sh

--- dockerrun.sh
+++ dockerrun.sh
@@ -3,3 +3,5 @@ docker rm tta_db tta_app;
 docker run --name tta_db -d -p 5433:5431 --network mynetwork -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=travelAnalyzer postgres -p 5431;
 docker build -t tta_app ./;
 docker run --name tta_app -d --network mynetwork tta_app;
+
+#docker-compose -p tta up --build
