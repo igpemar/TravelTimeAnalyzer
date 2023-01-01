80d1b9a4342af5a0dd47dff9e8f302a1b69fc69f
Ignacio V Perez <igp@lunar.app>
2023-01-01 17:37:53 +0100

    add: refactor dockerrun.sh

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,8 +1,5 @@
-REM docker network create -d bridge mynetwork
-docker stop tta_db;
-docker rm tta_db;
+docker stop tta_db tta_app;
+docker rm tta_db tta_app;
 docker run --name tta_db -d -p 5433:5431 --network mynetwork -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=travelAnalyzer postgres -p 5431;
-docker stop tta_app;
-docker rm tta_app;
 docker build -t tta_app ./;
 docker run --name tta_app -d --network mynetwork tta_app;
